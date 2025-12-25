39359201817e578e1bf80f0535f6a855d5ee4a85
bedirhan-1 <bedirhan9828@gmail.com>
2025-12-25 14:36:51 +0300

    lts

--- sanity.config.ts
+++ sanity.config.ts
@@ -8,7 +8,7 @@ import { visionTool } from '@sanity/vision'
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
 
-import { schemaTypes } from './sanity/schemas'
+import { schemaTypes } from './sanity/schemas/index'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
